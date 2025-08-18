71be6d4d392c939b56d09eb5faa70d8345ae59f9
Virginia Josefina Frias <virginiafriasunc2000@gmail.com>
2025-08-18 17:26:35 +0200

    First commit: Proyect initialization

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const PORT = 3030;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`Bienvenido a Tshirt-Store, corriendo en el puerto ${PORT}`);
+});
+
+app.listen(PORT || 3000, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
